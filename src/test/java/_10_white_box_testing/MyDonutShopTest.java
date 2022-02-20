06f79eda79630e52a9ff48d146efa4c38705f5bb
J1a4b4 <46267154+J1a4b4@users.noreply.github.com>
2022-02-20 12:22:58 -0800

    MyDonutShop

--- src/test/java/_10_white_box_testing/MyDonutShopTest.java
+++ src/test/java/_10_white_box_testing/MyDonutShopTest.java
@@ -13,6 +13,9 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
 
+import java.util.Collections;
+import java.util.List;
+
 class MyDonutShopTest {
 
     MyDonutShop myDonutShop;
@@ -25,37 +28,52 @@ class MyDonutShopTest {
     
     @Mock
     BakeryService bakeryService;
+    
+    @Mock
+    Order order;
 
     @BeforeEach
     void setUp() {
     	MockitoAnnotations.openMocks(this);
+    	List<Order> orders = Collections.singletonList(order);
+    	myDonutShop = new MyDonutShop(paymentService, deliveryService, bakeryService);
     }
 
     @Test
     void itShouldTakeDeliveryOrder() throws Exception {
         //given
-    	
+    	Order order = new Order("CUSTOMER_NAME", "CUSTOMER_PHONE_NUMBER", 1, 5.00, "CREDIT_CARD_NUMBER", true);
+    	when(paymentService.charge(order)).thenReturn(true);
+    	when(bakeryService.getDonutsRemaining()).thenReturn(50);
         //when
-
+    	myDonutShop.openForTheDay();
+    	myDonutShop.takeOrder(order);
         //then
+    	verify(deliveryService, times(1)).scheduleDelivery(order);
     }
 
     @Test
     void givenInsufficientDonutsRemaining_whenTakeOrder_thenThrowIllegalArgumentException() {
         //given
-
+    	Order order = new Order("CUSTOMER_NAME", "CUSTOMER_PHONE_NUMBER", 10, 5.00, "CREDIT_CARD_NUMBER", true);
+    	when(paymentService.charge(order)).thenReturn(true);
+    	when(bakeryService.getDonutsRemaining()).thenReturn(8);
         //when
-
+    	myDonutShop.openForTheDay();
         //then
+    	Throwable e = assertThrows(IllegalArgumentException.class, ()->{myDonutShop.takeOrder(order);});
+    	assertEquals(e.getMessage(), "Insufficient donuts remaining");
     }
 
     @Test
     void givenNotOpenForBusiness_whenTakeOrder_thenThrowIllegalStateException(){
         //given
-
+    	Order order = new Order("CUSTOMER_NAME", "CUSTOMER_PHONE_NUMBER", 1, 5.00, "CREDIT_CARD_NUMBER", true);
         //when
-
+    	myDonutShop.closeForTheDay();
         //then
+    	Throwable e = assertThrows(IllegalStateException.class, ()->{myDonutShop.takeOrder(order);});
+    	assertEquals(e.getMessage(), "Sorry we're currently closed");
     }
 
 }
